924ff78959861361ac4a1ef6ef897c3889616fb1
Gabriel Menezes <menezesx2k26@gmail.com>
2026-07-31 17:39:23 -0300

    fix: aplica contato do WhatsApp no ambiente publicado

---
+++ drizzle/0003_whatsapp_contact.sql
@@ -0,0 +1,6 @@
+UPDATE `site_settings`
+SET
+	`whatsapp_e164` = '554797821657',
+	`whatsapp_message` = 'Olá Maria! vim pelo seu site, tenho interesse no seu trabalho.',
+	`updated_at` = '2026-07-31T00:00:00.000Z'
+WHERE `id` = 1;
